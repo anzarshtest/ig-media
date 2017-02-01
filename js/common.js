fb976b4f1832282ecac2b36b6910111485434daf
anzarshtest <anzarshtest@mail.ru>
2017-02-01 15:46:40 +0300

    popups

--- js/common.js
+++ js/common.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
-    // alert(1);
+
+    $(".phone").mask("+ 7 (999) 999 - 99 - 99?");
+    
+    /*popup*/
+        $('.popup-with-move-anim').magnificPopup({
+            type: 'inline',
+
+            fixedContentPos: false,
+            fixedBgPos: true,
+
+            overflowY: 'auto',
+
+            closeBtnInside: true,
+            preloader: false,
+            
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-slide-bottom'
+        });
+    /*popup*/
+
+    /*ajax send*/
+    $('.az-send').submit(function(){
+        
+        return false;
+    });
+    /*ajax send*/
+
 });
 
 
